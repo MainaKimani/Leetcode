5c7f6674b34d544fbb4c24043700c74c7e93cc4e
Maina <alex.maina@savannahinformatics.com>
2023-10-10 14:46:55 +0300

    solution: Word Pattern py3

--- Word Pattern/solution.py
+++ Word Pattern/solution.py
@@ -3,26 +3,38 @@ Python3 used for this solution
 """
 
 class Solution:
+    """Create a funtion that accepts the pattern and string as params"""
     def wordPattern(self, pattern: str, s: str) -> bool:
-        # create a dictionary to store the mapping between characters and words
-        d = {}
-        # split the string into words
-        words = s.split()
-        # check if the number of characters in the pattern matches the number of words in the string
-        if len(pattern) != len(words):
+        """
+        Create a dictionary that stores the mapping between characters and words.
+        Why a dictionary? - Dictionaries cannot have two items with the same key
+        (Duplicate values will overwrite existing values).
+        """
+        mapping_dict = {}
+
+        """split the string into words and store them in an array"""
+        words_arr = s.split()
+
+        """
+        First check is whether the length of the string equates to the length of the array.
+        If not, return false.
+        """
+        if len(pattern) != len(words_arr):
             return False
-         # iterate over the pattern and the words
+        
+        """iterate over the pattern and the words"""
         for i in range(len(pattern)):
-            # check if the character is already in the dictionary
-            if pattern[i] in d:
-                # check if the word is the same as the one stored in the dictionary
-                if d[pattern[i]] != words[i]:
+            """start by checking if the character is already in the dictionary"""
+            if pattern[i] in mapping_dict:
+                """If it's present, check if the word in the array is the same as the one stored in the dictionary"""
+                if mapping_dict[pattern[i]] != words_arr[i]:
                     return False
             else:
-                # check if the word is already in the dictionary
-                if words[i] in d.values():
+                """check if the word in the array is already in the dictionary"""
+                if words_arr[i] in mapping_dict.values():
                     return False
-                # add the character and word to the dictionary
-                d[pattern[i]] = words[i]
-        # return True if all the characters and words match
+                """If both the character and word are not in the dictionary, add them to the dictionary"""
+                mapping_dict[pattern[i]] = words_arr[i]
+        
+        """return True if all the characters and words match"""
         return True
